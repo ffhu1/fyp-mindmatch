d6b26ece00d53b23acc443cb1266c7485cafdca7
ffhu1 <mail.hufangfang@gmail.com>
2020-06-24 04:33:32 +0100

    edited comments

--- mindmatch/matcher/models.py
+++ mindmatch/matcher/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.urls import reverse # Used to generate URLs by reversing the URL patterns
 
-# Create your models here.
 class Author(models.Model):
     """
     Model representing an academic (i.e. author of a paper)
--- mindmatch/matcher/views.py
+++ mindmatch/matcher/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
 from .models import *
 from .serializers import *
 from rest_framework import viewsets, generics
